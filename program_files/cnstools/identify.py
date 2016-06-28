79e7f9b80a070af7c2e923b6bdc52cfc314427d8
dauglyon <dauglyon@users.noreply.github.com>
2016-06-28 14:14:15 -0400

    runs a blast against each genome of each genomes' CNS regions, bug present because of 'N'-only sequences which have been identified as CNSs, these need to be eliminated, and a parameter made for what % of 'N's is allowed per sequence. Single Nucl CNS identification is still not determined to be a bug or not. The program now takes a parameter file and not a list of files, see sample_params.json for more info.

--- program_files/cnstools/identify.py
+++ program_files/cnstools/identify.py
@@ -1,5 +1,6 @@
 import progress_tracker as pt
 import subprocess
+import json
 
 from __consts import listOfModules
 for module in listOfModules:
@@ -7,56 +8,103 @@ for module in listOfModules:
         exec "from "+module+" import main as " + module
         
 
-def main(roasted_maf_files,gff3_files,out_foler):
-    #   maf_to_bed(roasted_maf_files) -> seq_beds w/maf_seq_ids
-    seq_beds = []
-    for file in roasted_maf_files:
-        seq_beds.append(out_foler+file.split("/")[-1].split(".maf")[0]+".bed")
-        maf_to_bed(file,seq_beds[-1])
-    #   gff3_to_bed(gff3_files,"CDS") -> cds_beds w/ alinged_to_cds_bed file
-    cds_beds = []
-    for file in gff3_files:
-        cds_beds.append(out_foler+file.split("/")[-1].split(".gff3")[0]+"_CDSs.bed")
-        gff3_to_bed(file,['CDS'],cds_beds[-1])
-    #   $bedtools.subtract(seq_beds - alinged_to_cds_bed) -> cns_beds w/maf_seq_ids
-    cns_beds = []
-    for i in range(len(seq_beds)):
-        cns_beds.append(out_foler+seq_beds[i].split("/")[-1].split(".bed")[0]+"_masked.bed")
-        process = subprocess.Popen("bedtools subtract -a "+seq_beds[i]+" -b "+cds_beds[i]+" > "+cns_beds[-1], shell=True)
+def main(data,out_folder):
+
+    process = subprocess.Popen("mkdir -p "+out_folder, shell=True)
+    process.wait()
+
+    #maf_to_bed(roasted_maf_file) -> seq_bed w/maf_seq_ids
+    header_print("Converting .maf file main sequence to .bed file locations.")
+    data['seq_bed'] = out_folder+data['maf_file'].split("/")[-1].split(".maf")[0]+".bed"
+    maf_to_bed(data['maf_file'],data['seq_bed'])
+
+    #gff3_to_bed(main_gff3,"CDS") -> cds_bed
+    header_print("Converting main .gff3 CDS ranges to .bed file locations.")
+    data['main_gff3'] = data['seqs'][0]['gff3_file_name']
+    data['cds_bed'] = out_folder+data['main_gff3'].split("/")[-1].split(".gff3")[0]+"_CDS.bed"
+    gff3_to_bed(data['main_gff3'],['CDS'],data['cds_bed'])
+
+    #$bedtools.subtract(seq_bed - cds_bed) -> cns_bed w/maf_seq_ids
+    header_print("Subtracting CDS seqs from alignment.") 
+    data['cns_bed'] = out_folder+data['seq_bed'].split("/")[-1].split(".bed")[0]+"_CNS.bed"
+    cmd = "bedtools subtract -a %s -b %s > %s" % (data['seq_bed'],data['cds_bed'],data['cns_bed'])
+    process = subprocess.Popen(cmd, shell=True)
+    process.wait()
+
+    #bed_maf_parse(cns_bed,roasted_maf_file) -> cns_maf w/cns_seq_ids
+    header_print("Making a new .maf of only CNSs")
+    data['cns_maf'] = out_folder+data['cns_bed'].split("/")[-1].split(".bed")[0]+".maf"
+    bed_maf_parse(data['cns_bed'],data['maf_file'],data['cns_maf'])
+
+    #maf_to_fasta(cns_maf) -> cns_fastas w/cns_seq_ids
+    header_print("Making fasta files from .maf")
+    data['cns_fasta_folder'] = out_folder+data['cns_maf'].split("/")[-1].split(".maf")[0]+"_fastas/"
+    process = subprocess.Popen("mkdir "+data['cns_fasta_folder'], shell=True)
+    process.wait()
+    for seq in data['seqs']:
+        seq['cns_fasta'] = data['cns_fasta_folder']+seq['maf_name']+".fasta"
+    maf_to_fasta(data['cns_maf'],data['cns_fasta_folder'])
+
+    #$makeblastdb(gff3_files) -> blasts_dbs
+    header_print("Building blast databases for each genome.")
+    data['blast_db_folder'] = out_folder+data['cns_maf'].split("/")[-1].split(".maf")[0]+"_blast_dbs/"
+    cmd = "mkdir %s" % (data['blast_db_folder'])
+    process = subprocess.Popen(cmd, shell=True)
+    process.wait()
+    for seq in data['seqs']:
+        if seq['genome_fasta_name']=="": 
+            break
+        genome_file = seq['genome_fasta_name']
+        seq['db_name'] = data['blast_db_folder']+seq['maf_name']+"_db"
+        cmd = "makeblastdb -in %s -out %s -dbtype nucl -hash_index" % (genome_file,seq['db_name'])
+        print 'builidng blast database for %s...' % (seq['maf_name'])
+        process = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE)
         process.wait()
-    #   !bed_maf_parse(cns_beds,roasted_maf_files) -> cns_maf w/cns_seq_ids
-    cns_mafs = []
-    for i in range(len(cns_beds)):
-        cns_mafs.append(out_foler+cns_beds[i].split("/")[-1].split(".bed")[0]+"_cns.maf")
-        bed_maf_parse(cns_beds[i],roasted_maf_files[i],cns_mafs[-1])
-    #   maf_to_fasta(cns_maf) -> cns_fastas w/cns_seq_ids
-    cns_fastas = []
-    for i in range(len(cns_mafs)):
-        cns_fastas.append(out_foler+cns_mafs[i].split("/")[-1].split(".maf")[0]+"_fastas/")
-        process = subprocess.Popen("mkdir "+cns_fastas[-1], shell=True)
+        if process.stderr!=None: 
+            for line in process.stderr.readlines(): print line
+        for line in process.stdout.readlines(): print line
+
+    #$blast(cns_fastas@blast_dbs) -> cns_blasts w/cns_seq_ids
+    header_print("Blasting CNSs for each genome.")
+    data['blast_results_folder'] = out_folder+data['cns_maf'].split("/")[-1].split(".maf")[0]+"_blast_results/"
+    process = subprocess.Popen("mkdir "+data['blast_results_folder'], shell=True)
+    process.wait()
+    for seq in data['seqs']:
+        if seq['genome_fasta_name']=="": 
+            break
+        print 'blasting CNSs in %s against %s...' % (seq['cns_fasta'],seq['db_name'])
+        depth = len(data['blast_results_folder'].split("/"))
+        up = "../"*depth
+        out = data['blast_results_folder']+seq['maf_name']+".txt"
+        cmd = "cd %s; blastn -db %s -query %s -out %s -outfmt 6; cd %s" % (data['blast_results_folder'],seq['db_name'].split("/")[-1],up+seq['cns_fasta'],up+out,up)
+        process = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE)
         process.wait()
-        maf_to_fasta(cns_mafs[i],cns_fastas[-1])
-    #   $makeblastdb(gff3_files) -> blasts_dbs
-        #"makeblastdb -in file -out name -dbtype nucl -hash_index"
-    #   $blast(cns_fastas@blast_dbs) -> cns_blasts w/cns_seq_ids
-        for i in range(len(cns_fastas)):
-            process = subprocess.Popen("find "+cns_fastas[i]+"*", shell=True,stdout=subprocess.PIPE)
-            process.wait()
-            files = [file.strip() for file in process.stdout.readlines()]
-    #   blast_to_bed(cns_blasts) -> cns_locs w/cns_seq_ids
-    #   gff3_to_bed(gff3_files,"gene") -> gene_beds
-    #   $bedtools.closest(cns_locs@gene_beds) -> cns_assoc_data w/distance & cns_seq_ids
-    #   !parse_assoc_info(cns_assoc_data) -> cns_assoc_info w/cns_seq_ids
-    #   OUTPUT:
-    #       cns alignment from cns_maf
-    #       location in each species from cns_locs
-    #       closest genes and distance for each species from cns_assoc_info
-    #       catagory from cns_assoc_info
+        if process.stderr!=None: 
+            for line in process.stderr.readlines(): print line
+        for line in process.stdout.readlines(): print line
+
+    #blast_to_bed(cns_blasts) -> cns_locs w/cns_seq_ids
+    #gff3_to_bed(gff3_files,"gene") -> gene_beds
+    #$bedtools.closest(cns_locs@gene_beds) -> cns_assoc_data w/distance & cns_seq_ids
+    #!parse_assoc_info(cns_assoc_data) -> cns_assoc_info w/cns_seq_ids
+    #OUTPUT:
+    #    cns alignment from cns_maf
+    #    location in each species from cns_locs
+    #    closest genes and distance for each species from cns_assoc_info
+    #    catagory from cns_assoc_info
     pass
 
+def header_print(header):
+    wall = "-"*(len(header)+2)
+    print "%s\n %s \n%s" % (wall,header,wall)
+    return
+
 def run(argv):
-    out_foler = argv[3] if argv[3].endswith("/") else argv[3]+"/"
-    main([argv[1]],[argv[2]],out_foler)
+    config = None
+    with open(argv[1]) as intructionJSON:
+        config = json.load(intructionJSON)
+    out_folder = argv[2] if argv[2].endswith("/") else argv[2]+"/"
+    main(config,out_folder)
 
 if __name__ == '__main__':
     import sys

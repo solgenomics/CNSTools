2e757caa01cfa583e8e85765a01002e3726cce99
David Lyon <dauglyon@users.noreply.github.com>
2016-10-12 16:46:55 -0400

    added check for python 2.7

--- source/cnstools/setup.py
+++ source/cnstools/setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+import sys
+if sys.version_info < (2,7) or not sys.version_info < (2,8):
+    sys.exit('cnstools requires python 2.7')
 setup(name='cnstools',
       version='0.1',
       description='Conserved noncoding sequence identification tools. Developed for the Mueller lab at Boyce Thompson Institute.',
